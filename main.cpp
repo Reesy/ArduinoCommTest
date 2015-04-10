90def582b01993775ed52f759927658fa47b750c
James Rees <James.rees@outlook.com>
2015-04-10 13:30:09 +0100

    Initial "hello, world" added as a base.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArduinoCommTest
+//
+//  Created by James Rees on 10/04/2015.
+//  Copyright (c) 2015 James Rees. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "H.ello, World!\n";
+    return 0;
+}
