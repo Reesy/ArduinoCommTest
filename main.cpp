7e9346d642633e4b96da794418a6e0e0049010ad
James Rees <James.rees@outlook.com>
2015-04-12 15:05:16 +0100

    Added some test code, may need to add a flush function, currently the buffer gets full.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,24 @@
 
 #include <iostream>
 
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "H.ello, World!\n";
-    return 0;
+        std::cout << "Hello, World!\n";
+    
+    
+   
+    
+
+    //const FILE *file;
+    FILE* file;
+    file = fopen("/dev/cu.usbmodem1411","w");  //Opening device file
+    for(int i = 0; i < 10000; i++){
+   // std::cout << "The file is: " << file << std::endl;
+    
+        fprintf(file,"%d", 9); //Writing to the file
+        
+    }
+    fclose(file);
+    
 }
